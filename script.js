b8241ebec2bce119fed56b96a359ca7f35a7d854
T1ramisyu <ckyhampac22@src.pshs.edu.ph>
2025-02-13 08:54:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function solvePopulationGrowth(){
+    let e = Math.E;
+    let initialPopulation = parseFloat(prompt('Enter the initial population: '));
+    let rateOfGrowth = parseFloat(prompt('Enter the rate of growth: '));
+    let timeInHours = parseFloat(prompt('Enter the time in hours: '));
+    let finalPopulation = Math.round(initialPopulation * Math.pow(Math.pow(e, rateOfGrowth), timeInHours)); 
+
+    let location = prompt('Enter the location of the monster: ');
+    let nameOfMonster = prompt('Enter the name of the monster: ');
+
+    document.getElementById('result').innerHTML = 'After 3 hours, the population of ' + location.concat(' ', nameOfMonster).toUpperCase() + ' has increased to ' + finalPopulation;
+}
